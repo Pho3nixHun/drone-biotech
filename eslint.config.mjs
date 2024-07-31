a0f15de51cb47831672344b31e1981e7e1b4c74d
Bence7 <jeneib13@gmail.com>
2024-07-31 15:10:48 +0200

    Correction.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -8,4 +8,4 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-]
+];
