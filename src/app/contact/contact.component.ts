f1cbe57888eb8a3fefb7dab57782d13c789459c6
Bence7 <jeneib13@gmail.com>
2024-08-02 16:28:37 +0200

    Contact component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FooterComponent],
   templateUrl: './contact.component.html',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      name: ['', [Validators.required]],
+      message: ['', [Validators.required]],
+    });
+  }
+  get name() {
+    return this.contactForm.get('name')?.value;
+  }
+  get email() {
+    return this.contactForm.get('email')?.value;
+  }
+
+  get message() {
+    return this.contactForm.get('message')?.value;
+  }
+
+  onSubmit() {
+    console.log(this.contactForm.value)
+    this.contactForm.reset()
+  }
+}
