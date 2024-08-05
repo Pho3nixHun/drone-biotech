ed935cc3606357de3037f76c6d1e020df74ac1e2
Bence7 <jeneib13@gmail.com>
2024-08-05 20:42:10 +0200

    Add lazy loading.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: LandingComponent,
+    loadComponent: () => import('./landing/landing.component').then((m) => m.LandingComponent),
   },
   {
     path: 'product/:id',
-    component: ProductComponent,
-
-    /*loadChildren: () => import('./product/product.component').then((m) => m.ProductComponent),*/
+    loadComponent: () => import('./product/product.component').then((m) => m.ProductComponent),
   },
 ];
