a9f784c2da29fa06516fe7c60b6ec393612bc9d9
Bence7 <jeneib13@gmail.com>
2024-08-05 17:53:41 +0200

    Component for products.

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html'
+})
+export class ProductComponent {
+
+}
