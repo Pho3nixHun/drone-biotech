274322b2b73c03f0229e4ae31b425c74cf3d236f
Bence7 <jeneib13@gmail.com>
2024-08-07 10:29:24 +0200

    Removing fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       spacing: {
-        'my_spacing': '45rem',
+        my_spacing: '45rem',
       },
-      fontFamily: {
-        'myjetbrains': ['JetBrains'],
-        'mybolditalic': ['BoldItalic'],
-        'myitalic': ['Italic'],
-        'mybold': ['Bold'],
-        'myregular': ['Regular'],
-      },
-      
     },
   },
   plugins: [],
-}
+};
