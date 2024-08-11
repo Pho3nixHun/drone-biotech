2b405d0c99c9b89627b2f3d00cbfbc48941f38b3
Bence7 <jeneib13@gmail.com>
2024-08-11 12:40:43 +0200

    Moving files

--- src/app/shared/components/product/product.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-product',
-  standalone: true,
-  imports: [],
-  templateUrl: './product.component.html',
-})
-export class ProductComponent {}
