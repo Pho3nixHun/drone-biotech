ce8be8d44dda891ab1db55b50fdcb4e53bd1824b
Bence7 <jeneib13@gmail.com>
2024-08-11 12:38:14 +0200

    Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@components/header/header.component';
 import { FooterComponent } from '@components/footer/footer.component';
+import { NavComponent } from '@components/nav/nav.component';
+import { LogoComponent } from '@components/logo/logo.component';
+import { NavItemComponent } from '@components/nav-item/nav-item.component';
+import { ScrollDirective } from '@directives/scroll.directive';
+import { NavItem } from '@interfaces/nav-item';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    LogoComponent,
+    NavItemComponent,
+    ScrollDirective,
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'drone-biotech-webapp';
+
+  navItems = signal<NavItem[]>([
+    { link: 'product-section', text: 'Products' },
+    { link: 'partner-section', text: 'Partners' },
+    { link: 'contact-section', text: 'Contacts' },
+  ]);
 }
---
+++ src/app/pages/landing-page/landing.page.component.ts
@@ -0,0 +1,60 @@
+import { Component, inject, signal } from '@angular/core';
+import { ContactComponent } from '@components/contact/contact.component';
+import { HeroComponent } from '@components/hero/hero.component';
+import { ScrollIndicatorComponent } from '@components/scroll-indicator/scroll-indicator.component';
+import { ProductItemComponent } from '@components/product-item/product-item.component';
+import { RouterLink } from '@angular/router';
+import { ProductListComponent } from '@components/product-list/product-list.component';
+import { ProductSectionComponent } from '@components/product-section/product-section.component';
+import { PartnerSectionComponent } from '@components/partner-section/partner-section.component';
+import { AppRouteSegment } from 'src/app/app-route-segment';
+import { Product } from '@interfaces/product';
+import { Partners } from '@interfaces/partners';
+import { PartnerListComponent } from '@components/partner-list/partner-list.component';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [
+    ContactComponent,
+    HeroComponent,
+    ScrollIndicatorComponent,
+    ProductItemComponent,
+    RouterLink,
+    ProductListComponent,
+    ProductSectionComponent,
+    PartnerSectionComponent,
+    PartnerListComponent,
+  ],
+  templateUrl: './landing.page.component.html',
+})
+export class LandingPageComponent {
+  link = AppRouteSegment.PRODUCT;
+
+  products = signal<Product[]>([
+    { title: 'Controller', imageSrc: 'assets/lepke.png' },
+    { title: 'RTU', imageSrc: 'assets/lepke.png' },
+    { title: 'Cloud and Mobile', imageSrc: 'assets/lepke.png' },
+  ]);
+
+  partners = signal<Partners[]>([
+    { imageSrc: 'assets/lepke.png', altText: 'Lepke' },
+    { imageSrc: 'assets/lepke.png', altText: 'Lepke' },
+    { imageSrc: 'assets/lepke.png', altText: 'Lepke' },
+    { imageSrc: 'assets/lepke.png', altText: 'Lepke' },
+  ]);
+
+  private readonly formBuilder = inject(FormBuilder);
+  protected readonly contactForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    name: ['', [Validators.required]],
+    message: ['', [Validators.required]],
+  });
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.contactForm.reset();
+    }
+  }
+}
