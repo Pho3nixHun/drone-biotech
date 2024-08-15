30d2cb8666b9500201c9b1e074ebc99ef4859c1b
Bence7 <jeneib13@gmail.com>
2024-08-15 22:51:18 +0200

    Navbar items comp

---
+++ src/app/shared/components/nav-item/nav-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-item.component.html',
+})
+export class NavItemComponent {
+
+}
