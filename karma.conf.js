57b1ceb753ea2f2ae7f024e555d2fc0b5d71b061
Bence7 <jeneib13@gmail.com>
2024-08-15 22:52:13 +0200

    Conf and custom style

--- karma.conf.js
+++ karma.conf.js
@@ -2,5 +2,10 @@ module.exports = function (config) {
   config.set({
     singleRun: true,
     autoWatch: false,
+    reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      type: 'html',
+      dir: 'coverage/',
+    },
   });
 };
