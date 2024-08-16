37f54536b871b36c972fbd79a84130cfc336fc6c
László Simon <Pho3nixHun@users.noreply.github.com>
2024-08-16 17:49:15 +0200

    Revert "Chore/landing page/header"

--- karma.conf.js
+++ karma.conf.js
@@ -2,10 +2,5 @@ module.exports = function (config) {
   config.set({
     singleRun: true,
     autoWatch: false,
-    reporters: ['progress', 'coverage'],
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/',
-    },
   });
 };
