52891236eb92fb060bfbe4fcdb58b37a6f55e573
Bence7 <jeneib13@gmail.com>
2024-08-20 14:07:23 +0200

    App component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have the 'drone-biotech-webapp' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('drone-biotech-webapp');
+    expect(component.title).toEqual('drone-biotech-webapp');
+  });
+
+  it('should render app-header, app-logo, and app-nav components', () => {
+    expect(compiled.querySelector('app-header')).toBeTruthy();
+    expect(compiled.querySelector('app-logo')).toBeTruthy();
+    expect(compiled.querySelector('app-nav')).toBeTruthy();
+  });
+
+  it('should render the correct navigation titles', () => {
+    const anchors = compiled.querySelectorAll('app-nav a');
+    expect(anchors.length).toBe(4);
+    expect(anchors[0].textContent).toContain('Home');
+    expect(anchors[1].textContent).toContain('Products');
+    expect(anchors[2].textContent).toContain('Partners');
+    expect(anchors[3].textContent).toContain('Contacts');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/components/header/header.component';
-import { LogoComponent } from './shared/components/logo/logo.component';
-import { NavComponent } from './shared/components/nav/nav.component';
-import { NavItem } from './shared/interfaces/nav-item';
+import { HeaderComponent } from '@components/header/header.component';
+import { LogoComponent } from '@components/logo/logo.component';
+import { NavComponent } from '@components/nav/nav.component';
+import { LogoVM } from '@interfaces/logo-vm';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, LogoComponent, NavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'drone-biotech-webapp';
 
-  navItems = signal<NavItem[]>([
-    { link: 'product-section', text: 'Products' },
-    { link: 'partner-section', text: 'Partners' },
-    { link: 'contact-section', text: 'Contacts' },
-  ]);
+  logoVM = signal<LogoVM>({ imageSrc: 'assets/phoenix.jpg', routerLink: '' });
 }
