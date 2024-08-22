0460235e1d1442efe9ecf53515434fdab3d5915f
Bence7 <jeneib13@gmail.com>
2024-08-22 22:39:14 +0200

    Testing

--- src/app/shared/components/header/components/nav/components/nav-item/nav-item.component.spec.ts
+++ src/app/shared/components/header/components/nav/components/nav-item/nav-item.component.spec.ts
@@ -1,23 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NavItemComponent } from './nav-item.component';
+import { Component } from '@angular/core';
+
+@Component({
+  template: `<app-nav-item>Home</app-nav-item>`,
+})
+class TestHostComponent {}
 
 describe('NavItemComponent', () => {
-  let component: NavItemComponent;
-  let fixture: ComponentFixture<NavItemComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavItemComponent]
-    })
-    .compileComponents();
+      declarations: [TestHostComponent],
+      imports: [NavItemComponent, NavItemComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(NavItemComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("should project the 'Home' title inside the <app-nav-item>", () => {
+    const navItemElement = compiled.querySelector('app-nav-item');
+    expect(navItemElement?.textContent?.trim()).toBe('Home');
   });
 });
