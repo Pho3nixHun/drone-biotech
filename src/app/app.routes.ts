b3f0140535e9aebdee186ece63fd656d9076926b
Bence7 <jeneib13@gmail.com>
2024-08-30 11:04:52 +0000

    Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
+import { AppRouteSegment } from './app-route-segment';
 
 export const routes: Routes = [
   {
     path: '',
     loadComponent: () =>
       import('./pages/landing.page/landing.page.component').then((m) => m.LandingPageComponent),
+  },
+
+  {
+    path: AppRouteSegment.PRODUCT + '/:id',
+    loadComponent: () =>
+      import('./pages/product-item.page/product-item.page.component').then(
+        (m) => m.ProductItemPageComponent,
+      ),
   },
 ];
