18312fb764d1deed22124b169ec866584e91b1dc
Bence7 <jeneib13@gmail.com>
2024-08-30 11:06:46 +0000

    Landing page

--- src/app/pages/landing.page/landing.page.component.spec.ts
+++ src/app/pages/landing.page/landing.page.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LandingPageComponent } from './landing.page.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +9,7 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingPageComponent],
+      imports: [LandingPageComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LandingPageComponent);
@@ -26,9 +27,13 @@ describe('LandingPageComponent', () => {
   });
 
   it('should render the correct navigation titles', () => {
-    const anchors = compiled.querySelectorAll('a');
+    const anchors = compiled.querySelectorAll('app-hero a');
     expect(anchors.length).toBe(2);
     expect(anchors[0].textContent).toContain('Contact Us');
     expect(anchors[1].textContent).toContain('Learn More');
+  });
+
+  it('should render the <app-product>', () => {
+    expect(compiled.querySelector('app-product')).toBeTruthy();
   });
 });
