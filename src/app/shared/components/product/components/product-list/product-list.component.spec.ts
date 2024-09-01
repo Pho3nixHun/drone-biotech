5fa30ad8b950d67f9883226dfc64eb2e09cabb31
Bence7 <jeneib13@gmail.com>
2024-09-01 09:58:13 +0000

    Product List comp

--- src/app/shared/components/product/components/product-list/product-list.component.spec.ts
+++ src/app/shared/components/product/components/product-list/product-list.component.spec.ts
@@ -1,40 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductListComponent } from './product-list.component';
-import { AppRouteSegment } from 'src/app/app-route-segment';
-import { ProductListComponentVM } from './product-list.component.vm';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { ProductItemComponent } from '../product-item/product-item.component';
 
+@Component({
+  template: `
+    <app-product-list>
+      <app-product-item></app-product-item>
+      <app-product-item>This should not been projected</app-product-item>
+      <div>This should not been projected</div>
+    </app-product-list>
+  `,
+})
+class TestHostComponent {}
 describe('ProductListComponent', () => {
-  let component: ProductListComponent;
-  let fixture: ComponentFixture<ProductListComponent>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
   let compiled: HTMLElement;
-  const vm: ProductListComponentVM = {
-    productItems: [
-      {
-        title: 'Controller',
-        description: 'Description',
-        imageSrc: 'test.jpg',
-      },
-      {
-        title: 'RTU',
-        description: 'Description',
-        imageSrc: 'test.jpg',
-      },
-      {
-        title: 'Cloud and Mobile',
-        description: 'Description',
-        imageSrc: 'test.jpg',
-      },
-    ],
-    link: AppRouteSegment.PRODUCT,
-  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductListComponent, RouterTestingModule],
+      imports: [ProductListComponent, ProductItemComponent],
+      declarations: [TestHostComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ProductListComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
     compiled = fixture.debugElement.nativeElement;
@@ -44,10 +34,11 @@ describe('ProductListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should create the <app-product-item> elements', () => {
-    fixture.componentRef.setInput('vm', vm);
-    fixture.detectChanges();
-    const appProductItemElements = compiled.querySelectorAll('app-product-item');
-    expect(appProductItemElements.length).toEqual(vm.productItems.length);
+  it('should contain the <app-product-item> elements and ignore other elements', () => {
+    const productItems = compiled.querySelectorAll('app-product-list app-product-item');
+    expect(productItems.length).toBe(2);
+
+    const divItems = compiled.querySelectorAll('app-product-list div div');
+    expect(divItems.length).toBe(0);
   });
 });
--- src/app/shared/components/product/components/product-list/product-list.component.ts
+++ src/app/shared/components/product/components/product-list/product-list.component.ts
@@ -3,6 +3,26 @@ import { ProductItemComponent } from '../product-item/product-item.component';
 import { RouterLink } from '@angular/router';
 import { ProductListComponentVM } from './product-list.component.vm';
 
+/**
+ * ProductListComponent
+ *
+ * Type: Presentational
+ *
+ * Scope:
+ * - Responsible for rendering a list of products.
+ * - Delegates the rendering of individual products to `ProductItem` components.
+ * - Manages the layout and styling of the product list.
+ *
+ * Out-of-Scope:
+ * - Should not handle the data fetching or business logic related to product details.
+ * - Should not be responsible for manipulating or transforming product data.
+ * - No logic related to the individual `ProductItem` components' behavior.
+ *
+ * Purpose(optional):
+ * To provide a clear and simple presentation of a list of products, utilizing `ProductItem` components
+ * for each product's rendering.
+ */
+
 @Component({
   selector: 'app-product-list',
   standalone: true,
