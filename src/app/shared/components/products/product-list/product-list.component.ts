2ff1ac88f774469b02837e7a0b52473599ab0a92
Bence7 <jeneib13@gmail.com>
2024-09-01 21:14:17 +0000

    Product list comp

---
+++ src/app/shared/components/products/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+/**
+ * ProductListComponent
+ *
+ * Type: Presentational
+ *
+ * Scope:
+ * - Responsible for rendering a list of products.
+ * - Manages the layout and styling of the product list.
+ *
+ * Out-of-Scope:
+ * - Should not handle the data fetching or business logic related to product details.
+ * - Should not be responsible for manipulating or transforming product data.
+ *
+ * Purpose(optional):
+ * - To create a simple layout that contains `ProductItem`-s.
+ */
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+})
+export class ProductListComponent {}
