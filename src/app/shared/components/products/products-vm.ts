3459b267629796b84e2d16cff5f7d41b3271dc32
Bence7 <jeneib13@gmail.com>
2024-09-01 21:22:00 +0000

    ProductsVM

---
+++ src/app/shared/components/products/products-vm.ts
@@ -0,0 +1,7 @@
+import { ProductItemComponentVM } from '@components/products/product-item/product-item.component.vm';
+
+export interface ProductsVM {
+  title: string;
+  link: string;
+  productItems: ProductItemComponentVM[];
+}
