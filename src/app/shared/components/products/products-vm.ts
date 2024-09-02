9ff4bb2f17af22caa68022a5441a0f4e2adbbb28
Bence7 <jeneib13@gmail.com>
2024-09-02 16:02:36 +0000

    Moving files

--- src/app/shared/components/products/products-vm.ts
+++
@@ -1,7 +0,0 @@
-import { ProductItemComponentVM } from '@components/products/product-item/product-item.component.vm';
-
-export interface ProductsVM {
-  title: string;
-  link?: string;
-  productItems: ProductItemComponentVM[];
-}
