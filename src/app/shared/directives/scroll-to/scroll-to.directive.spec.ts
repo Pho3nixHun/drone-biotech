41d9e04eb72d344b99d648937e49e3f6453e01b4
Bence7 <jeneib13@gmail.com>
2024-09-11 17:30:01 +0000

    Directive

--- src/app/shared/directives/scroll-to/scroll-to.directive.spec.ts
+++ src/app/shared/directives/scroll-to/scroll-to.directive.spec.ts
@@ -5,17 +5,17 @@ import { ScrollToDirective } from './scroll-to.directive';
 
 @Component({
   template: `
-    <a href="#section1" appScroll>Scroll to Section 1</a>
-    <div id="section1">Section 1</div>
+    <div appScrollTo="section1">Scroll to Section 1</div>
+    <section id="section1">Section 1</section>
   `,
   standalone: true,
   imports: [ScrollToDirective],
 })
 class TestHostComponent {}
 
-describe('ScrollDirective', () => {
+describe('ScrollToDirective', () => {
   let fixture: ComponentFixture<TestHostComponent>;
-  let anchorDebugElement: DebugElement;
+  let divDebugElement: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TestHostComponent],
@@ -23,24 +23,23 @@ describe('ScrollDirective', () => {
 
     fixture = TestBed.createComponent(TestHostComponent);
     fixture.detectChanges();
+    divDebugElement = fixture.debugElement.query(By.directive(ScrollToDirective));
   });
 
-  it('should create an instance', () => {
-    const directive = new ScrollToDirective();
-    expect(directive).toBeTruthy();
+  it('should create an instance of the directive', () => {
+    const directiveInstance = divDebugElement.injector.get(ScrollToDirective);
+    expect(directiveInstance).toBeTruthy();
   });
 
   it('should prevent the default action and scroll to the target element', () => {
-    anchorDebugElement = fixture.debugElement.query(By.css('a'));
-
     const event = new MouseEvent('click');
     spyOn(event, 'preventDefault');
 
     const targetElement = document.getElementById('section1');
     if (targetElement) {
       const scrollSpy = spyOn(targetElement, 'scrollIntoView');
-      anchorDebugElement.nativeElement.click();
-      expect(scrollSpy).toHaveBeenCalledWith({ behavior: 'smooth' });
+      divDebugElement.nativeElement.click();
+      expect(scrollSpy).toHaveBeenCalledWith({ behavior: 'smooth', block: 'start' });
     }
   });
 });
