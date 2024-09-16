9ebe6f1cc8562353e99131a6c6c1ac6de373acd3
Bence7 <jeneib13@gmail.com>
2024-09-16 10:40:42 +0000

    Firebase integration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore(),
