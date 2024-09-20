1fb5c9bf679f49e35c681e95402f9e34b49ed060
Bence7 <jeneib13@gmail.com>
2024-09-20 12:30:13 +0000

    Test Transloco

--- src/app/shared/components/hero/hero.component.spec.ts
+++ src/app/shared/components/hero/hero.component.spec.ts
@@ -1,37 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroComponent } from './hero.component';
-import { HeroComponentVM } from './hero.component.vm';
+import { HeroVM } from './hero-vm.model';
 import { Component, signal } from '@angular/core';
+import { getTranslocoModule } from 'transloco-testing.module';
+import { TranslocoService } from '@jsverse/transloco';
 
 @Component({
   template: `
-    <app-hero [vm]="vm()">
-      <h1 class="testH1" id="test">The future of irrigation is now</h1>
-      <p class="testP">IRRIOT is a completely wireless precision irrigation automation system</p>
-      <a class="testAnchor" href="">Contact Us</a>
-      <button class="testButton">Test button</button>
-      <div class="testDiv">Should not be projected</div>
-    </app-hero>
+    @if (vm(); as vm) {
+      <app-hero [vm]="vm">
+        <h1 class="testH1" id="test">{{ vm.titles[0] | transloco }}</h1>
+        <p class="testP">{{ vm.titles[1] }}</p>
+        <a class="testAnchor" href="">{{ vm.buttonTitles[0] | transloco }}</a>
+        <button class="testButton">{{ vm.buttonTitles[1] | transloco }}</button>
+        <div class="testDiv">Should not be projected</div>
+      </app-hero>
+    }
   `,
 })
 class TestHostComponent {
-  vm = signal<HeroComponentVM | null>({
+  vm = signal<HeroVM>({
     backgroundImageSrc: 'assets/farming.jpg',
+    titles: ['LandingPage.hero.0.title', 'LandingPage.hero.1.title'],
+    buttonTitles: ['LandingPage.hero.0.buttonTitle', 'LandingPage.hero.1.buttonTitle'],
   });
 }
 describe('HeroComponent', () => {
   let component: TestHostComponent;
   let fixture: ComponentFixture<TestHostComponent>;
   let compiled: HTMLElement;
+  let translocoService: TranslocoService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroComponent],
+      imports: [HeroComponent, getTranslocoModule()],
       declarations: [TestHostComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
+    translocoService = TestBed.inject(TranslocoService);
     fixture.detectChanges();
     compiled = fixture.debugElement.nativeElement;
   });
@@ -62,5 +70,21 @@ describe('HeroComponent', () => {
     expect(sectionElement?.style.backgroundImage).toBe(
       `url("${component.vm()?.backgroundImageSrc}")`,
     );
+  });
+
+  it('should get input data (backgroundImageSrc)', () => {
+    const sectionElement: HTMLElement | null = compiled.querySelector('section');
+    expect(sectionElement?.style.backgroundImage).toBe(
+      `url("${component.vm()?.backgroundImageSrc}")`,
+    );
+  });
+
+  it('should translate correctly', () => {
+    const h1Element: HTMLElement | null = compiled.querySelector('.testH1');
+    if (h1Element?.innerText)
+      expect(translocoService.translate(component.vm()?.titles[0])).toBe(h1Element?.innerText);
+
+    const pElement: HTMLElement | null = compiled.querySelector('.testP');
+    if (pElement?.innerText) expect(component.vm()?.titles[1]).toBe(pElement?.innerText);
   });
 });
