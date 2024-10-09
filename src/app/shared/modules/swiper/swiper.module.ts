ab238ce772d5329ed03b5b6e11784088bcb8f83d
Laszlo Simon <pho3nixhun@proton.me>
2024-10-09 20:50:43 +0200

    fix: ci test

--- src/app/shared/modules/swiper/swiper.module.ts
+++ src/app/shared/modules/swiper/swiper.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import { register } from 'swiper/element-bundle';
 
 @NgModule()
