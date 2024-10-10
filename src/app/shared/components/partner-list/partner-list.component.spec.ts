59be69420f890d2fa6a44f0a980a0117ff08262c
Bence7 <jeneib13@gmail.com>
2024-10-10 23:48:56 +0200

    Partner List comp

--- src/app/shared/components/partner-list/partner-list.component.spec.ts
+++ src/app/shared/components/partner-list/partner-list.component.spec.ts
@@ -1,51 +1,59 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PartnerListComponent } from './partner-list.component';
 import { Component } from '@angular/core';
 import { PartnerLogoComponent } from './components/partner-logo/partner-logo.component';
+import { PartnerLogoVM } from './components/partner-logo/partner-logo-vm.model';
+import { getTranslocoModule } from 'transloco-testing.module';
+
+const en = {
+  altText: 'logo',
+};
 
 @Component({
   template: `
     <app-partner-list>
-      <app-partner-logo></app-partner-logo>
-      <app-partner-logo></app-partner-logo>
+      <app-partner-logo [vm]="vm" />
+      <app-partner-logo [vm]="vm" />
       <app-partner-list>Should not be projected</app-partner-list>
     </app-partner-list>
   `,
 })
-class TestHostComponent {}
+class TestHostComponent {
+  vm: PartnerLogoVM = {
+    altTextKey: en.altText,
+    imageSrc: 'assets/lepke.jpg',
+  };
+}
 
 describe('PartnerListComponent', () => {
-  let component: TestHostComponent;
   let fixture: ComponentFixture<TestHostComponent>;
   let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PartnerListComponent, PartnerLogoComponent],
+      imports: [
+        PartnerListComponent,
+        PartnerLogoComponent,
+        getTranslocoModule({
+          langs: { en: en },
+          translocoConfig: { availableLangs: ['en'], defaultLang: 'en' },
+        }),
+      ],
       declarations: [TestHostComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TestHostComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
     compiled = fixture.debugElement.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
   //Snapshot test
-  it('should project the <app-partner-logo> and ignore other elements', () => {
+  it('should render the template when the VM is provided', () => {
     //Arrange
-    const logoItems = compiled.querySelectorAll('app-partner-list app-partner-logo');
-    const partnerList = compiled.querySelectorAll('app-partner-list app-partner-list');
 
     //Act
 
     //Assert
-    expect(logoItems.length).toBe(2);
-    expect(partnerList.length).toBe(0);
+    expect(compiled).toMatchSnapshot();
   });
 });
