36af9ce8c668bb4f232718abb48b66ad09a07018
Bence7 <jeneib13@gmail.com>
2024-11-03 22:00:46 +0000

    Routing, guards

--- src/app/pages/products-page/products-routing.module.ts
+++ src/app/pages/products-page/products-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductsPageComponent } from './products-page.component';
+import { AuthGuard } from 'src/app/shared/guards/auth/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: ProductsPageComponent }, // /products/ route
