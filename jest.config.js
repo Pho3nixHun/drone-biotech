4ae9035e80c94a613e2534aad61666e6547d0eba
Bence7 <jeneib13@gmail.com>
2024-11-12 08:24:19 +0000

    fixing

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
     rootDir: './',
     modulePaths: ['<rootDir>'],
     moduleNameMapper: {
-        '^flat': 'node_modules/flat/index.js',
         '^@components/(.*)$': '<rootDir>/src/app/shared/components/$1',
         '^@directives/(.*)$': '<rootDir>/src/app/shared/directives/$1',
         '^@modules/(.*)$': '<rootDir>/src/app/shared/modules/$1',
