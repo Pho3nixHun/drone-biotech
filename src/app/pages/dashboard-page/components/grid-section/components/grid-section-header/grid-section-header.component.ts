bcb76d86c859961ef4c4ecd329d0c5ccf4e4a91b
Bence7 <jeneib13@gmail.com>
2025-06-13 10:51:31 +0000

    feat/dashboard

---
+++ src/app/pages/dashboard-page/components/grid-section/components/grid-section-header/grid-section-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-grid-section-header',
+    imports: [],
+    templateUrl: './grid-section-header.component.html',
+})
+export class GridSectionHeaderComponent {}
