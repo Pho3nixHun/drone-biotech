6bd94f0115f9bf528e4dd5af1d3d606ff8534f63
Bence7 <jeneib13@gmail.com>
2025-07-24 09:18:09 +0000

    Header component

---
+++ src/app/pages/orders-new-page/components/areas-data-form-control/components/section-header/section-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-section-header',
+    imports: [],
+    templateUrl: './section-header.component.html',
+})
+export class SectionHeaderComponent {}
