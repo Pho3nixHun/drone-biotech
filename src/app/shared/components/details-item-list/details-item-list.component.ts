24c2dac5491836018db0d149c10d9db05a971522
Bence7 <jeneib13@gmail.com>
2025-07-29 08:41:46 +0000

    Details item

---
+++ src/app/shared/components/details-item-list/details-item-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-details-item-list',
+    imports: [],
+    templateUrl: './details-item-list.component.html',
+})
+export class DetailsItemListComponent {}
