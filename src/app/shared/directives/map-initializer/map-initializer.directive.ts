f0b111ed18dfee6c601eca05f27044ae9b5cd827
Bence7 <jeneib13@gmail.com>
2025-07-31 20:52:05 +0000

    Directives

--- src/app/shared/directives/map-initializer/map-initializer.directive.ts
+++ src/app/shared/directives/map-initializer/map-initializer.directive.ts
@@ -6,6 +6,7 @@ import {
     input,
     WritableSignal,
 } from '@angular/core';
+import { Coordinates } from '@stores/location/location.model';
 
 @Directive({
     selector: '[appMapInitializer]',
@@ -13,12 +14,16 @@ import {
 export class MapInitializerDirective implements AfterViewInit {
     private readonly elementRef = inject(ElementRef<HTMLDivElement>);
 
-    public signal = input.required<WritableSignal<google.maps.Map | null>>();
+    public mapSignal = input.required<WritableSignal<google.maps.Map | null>>();
     public mapOptions = input.required<google.maps.MapOptions>();
+    public mapCenter = input.required<Coordinates>();
 
     public ngAfterViewInit() {
+        const mapSignal = this.mapSignal()();
+        if (mapSignal) return;
         const map = new google.maps.Map(this.elementRef.nativeElement);
         map.setOptions(this.mapOptions());
-        return this.signal().set(map);
+        map.setCenter(this.mapCenter());
+        return this.mapSignal().set(map);
     }
 }
