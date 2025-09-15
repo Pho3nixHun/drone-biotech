8ebba80018c753f16f6bf29d9e95fc1fba69e988
Bence7 <jeneib13@gmail.com>
2025-09-15 09:47:30 +0000

    Confirmation dialog

--- src/app/shared/components/confirmation-dialog/confirmation-dialog.model.ts
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.model.ts
@@ -1,25 +1,16 @@
-import { ButtonXVM } from '@components/button/button.model';
-import { DialogLayoutVM } from '@components/dialog-layout/dialog-layout.model';
-import { DialogReason } from '@services/dialog/dialog.service';
-import { isObject } from '@utils/is-object.typeguard';
+import { DialogLayoutXVM } from '@components/dialog-layout/dialog-layout.component';
 
-export interface ConfirmationDialogVM extends DialogLayoutVM {
-    type: 'confirmationDialogVM';
+export interface ConfirmationDialogVM extends DialogLayoutXVM {
     confirmTextKey: string;
-    confirmButtonXVM: ButtonXVM;
-    cancelButtonXVM: ButtonXVM;
 }
 
-export interface SubmitReason extends DialogReason {
-    reasonType: 'submit';
+interface ConfirmationDialogSubmitResponse {
+    type: 'submit';
 }
-export interface CancelReason extends DialogReason {
-    reasonType: 'cancel';
+interface ConfirmationDialogCancelResponse {
+    type: 'cancel';
 }
 
-export type ConfirmationDialogReason = SubmitReason | CancelReason;
-
-export const isConfirmationDialogVM = (
-    vm: unknown
-): vm is ConfirmationDialogVM =>
-    isObject(vm) && 'type' in vm && vm.type === 'confirmationDialogVM';
+export type ConfirmationDialogResponse =
+    | ConfirmationDialogSubmitResponse
+    | ConfirmationDialogCancelResponse;
