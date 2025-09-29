19a187fab825b86bec2005ba6e4addef60d834f7
Bence7 <jeneib13@gmail.com>
2025-09-29 13:08:14 +0000

    Orders new page

--- src/app/pages/orders-new-page/orders-new-page.service.ts
+++ src/app/pages/orders-new-page/orders-new-page.service.ts
@@ -1,5 +1,5 @@
 import { computed, inject, Injectable, Signal } from '@angular/core';
-import { OrdersNewPageVM } from './orders-new-page-vm.model';
+import { OrdersNewPageVM } from './orders-new-page.model';
 import { ordersNewPageVMDefault } from './orders-new-page.mock';
 import { injectDispatch } from '@ngrx/signals/events';
 import { LocationStore } from '@stores/location/location.store';
@@ -14,28 +14,15 @@ export class OrdersNewPageService {
     private readonly dispatchLocation = this.locationEvents.getLocation();
 
     private readonly vm = computed<OrdersNewPageVM>(() => {
-        const defaultCenter = this.locationStore.actualPosition();
+        const actualPosition = this.locationStore.actualPosition();
         return {
             ...ordersNewPageVMDefault,
             areasDataFormControlVM: {
                 ...ordersNewPageVMDefault.areasDataFormControlVM,
-                addAreaDataDialogVM: {
+                areaDataDialogVM: {
                     ...ordersNewPageVMDefault.areasDataFormControlVM
-                        .addAreaDataDialogVM,
-                    mapFormControlVM: {
-                        ...ordersNewPageVMDefault.areasDataFormControlVM
-                            .addAreaDataDialogVM.mapFormControlVM,
-                        defaultCenter,
-                    },
-                },
-                editAreaDataDialogVM: {
-                    ...ordersNewPageVMDefault.areasDataFormControlVM
-                        .editAreaDataDialogVM,
-                    mapFormControlVM: {
-                        ...ordersNewPageVMDefault.areasDataFormControlVM
-                            .addAreaDataDialogVM.mapFormControlVM,
-                        defaultCenter,
-                    },
+                        .areaDataDialogVM,
+                    actualPosition,
                 },
             },
         };
