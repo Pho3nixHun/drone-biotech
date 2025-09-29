94646b006f93d1086b3451d53abf44344cad0345
Bence7 <jeneib13@gmail.com>
2025-09-29 13:03:07 +0000

    Configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{html,ts}'],
-    theme: {
-        extend: {
-            width: {
-                300: '75rem', // 1200px
-                230: '57.5rem', // 920px
-                172: '43rem', // 688px (md)
-                140: '35rem', // 560px (sm)
-                96: '24rem', // 384px
-            },
-            height: {
-                200: '50rem', //800px
-            },
-        },
-    },
+    theme: {},
 };
