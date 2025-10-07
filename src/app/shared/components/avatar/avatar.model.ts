9c9b421f9da35c433491cddfcdb6d45e8cd35c1d
Bence7 <jeneib13@gmail.com>
2025-10-07 07:07:44 +0000

    Refactor avatar comp

--- src/app/shared/components/avatar/avatar.model.ts
+++ src/app/shared/components/avatar/avatar.model.ts
@@ -1,59 +1,22 @@
 import { WithImage } from '@interfaces/with-image.interface';
-import { User } from 'src/app/pages/order-details-page/order-details-page.model';
 
-const MAX_INITIALS = 3;
-
-interface AvatarXVMWithInitials {
+interface AvatarVMWithInitials {
     type: 'withInitials';
     initials: string;
-    colors: CSSStyles;
 }
 
-interface AvatarXVMWithImage extends WithImage {
+interface AvatarVMWithImage extends WithImage {
     type: 'withImage';
 }
 
-export type AvatarXVM = AvatarXVMWithInitials | AvatarXVMWithImage;
+export type AvatarVM = AvatarVMWithInitials | AvatarVMWithImage;
 
-export const mapAvatarXVM = (user: User): AvatarXVM => {
-    const initials = user.name
-        .split(' ')
-        .map((word) => word[0])
-        .join('')
-        .toUpperCase()
-        .slice(0, MAX_INITIALS);
-
-    return user.photoUrl
-        ? {
-              type: 'withImage',
-              imageSrc: user.photoUrl,
-              altTextKey: `${initials}`,
-          }
-        : {
-              type: 'withInitials',
-              initials,
-              colors: mapInitialsToCSSStyles(initials),
-          };
-};
-
-export const mapInitialsToCSSStyles = (initials: string): CSSStyles => {
-    const colors = [
-        CSSStyles.BLUE,
-        CSSStyles.GREEN,
-        CSSStyles.PURPLE,
-        CSSStyles.RED,
-        CSSStyles.YELLOW,
-    ];
+export const mapInitialsToRemainder = (
+    initials: string,
+    length: number
+): number => {
     const sum = initials
         .split('')
         .reduce((acc, curr) => acc + curr.charCodeAt(0), 0);
-    return colors[sum % colors.length];
+    return sum % length;
 };
-
-export enum CSSStyles {
-    RED = 'text-red-500 *:bg-red-200',
-    GREEN = 'text-green-500 *:bg-green-200',
-    BLUE = 'text-blue-500 *:bg-blue-200',
-    YELLOW = 'text-yellow-500 *:bg-yellow-200',
-    PURPLE = 'text-purple-500 *:bg-purple-200',
-}
