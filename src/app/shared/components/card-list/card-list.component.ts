fba0b8c0a0cd5060ab7a5c3c94dcad18d31aca56
Bence7 <jeneib13@gmail.com>
2025-10-10 07:57:35 +0000

    Add card and card list components with corresponding templates

---
+++ src/app/shared/components/card-list/card-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-card-list',
+    imports: [],
+    templateUrl: './card-list.component.html',
+})
+export class CardListComponent {}
