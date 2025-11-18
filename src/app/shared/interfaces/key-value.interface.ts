287df6a175b80146dd738011c151b3d2aff35ac2
Bence7 <jeneib13@gmail.com>
2025-11-18 17:22:04 +0000

    refactor: remove KeyValueComponent and related HTML, update badge and input components' snapshots

---
+++ src/app/shared/interfaces/key-value.interface.ts
@@ -0,0 +1,7 @@
+import { KeyXVM } from '@components/key/key.component';
+import { ValueVM } from '@components/value/value.component';
+
+export interface KeyValueXVM {
+    keyXVM: KeyXVM;
+    valueVM: ValueVM;
+}
